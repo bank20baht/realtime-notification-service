add1cc9ad047a701801480b6389576b3cc072334
Nattapong Promthong <banker.z412@gmail.com>
2024-02-14 12:29:44 +0700

    feat: init react router

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <App />,
+  },
+]);
+
+const rootElement = document.getElementById("root");
+
+if (rootElement !== null) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Could not find the root element");
+}
