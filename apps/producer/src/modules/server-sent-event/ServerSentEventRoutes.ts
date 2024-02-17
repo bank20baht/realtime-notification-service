1a8f10b5decf0f5309919b55aa95edd610e29afd
Nattapong Promthong <banker.z412@gmail.com>
2024-02-17 17:51:59 +0700

    refactor: change server sent event to class

--- apps/producer/src/modules/server-sent-event/ServerSentEventRoutes.ts
+++ apps/producer/src/modules/server-sent-event/ServerSentEventRoutes.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { getEvents } from "./ServerSentEventController";
+import { ServerSentEventController } from "./ServerSentEventController";
 
 const router = express.Router();
-
+const serverSentEventController = new ServerSentEventController();
 router.get("/check", (req, res) => {
   res.json({ message: "that work" });
 });
 
-router.get("/events/:user_id/:group_id", getEvents);
+router.get(
+  "/events/:user_id/:group_id",
+  serverSentEventController.getNotification.bind(serverSentEventController)
+);
 
 export default router;
